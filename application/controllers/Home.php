83e49742205df9d773654a8ea3efa11bd042d81a
lab <darsoulmone.123@gmail.com>
2016-11-26 16:14:34 +0700

    rhd

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -8,8 +8,17 @@
  */
 class Home extends  CI_Controller
 {
-   public  function  index() {
-        echo  "hello" ;
+   public  function  index()
+   {
+       $this->load->view("layout/header_view");
+       $this->load->view('home_view');
+       $this ->load ->view ('layout/foot_view.php');
    }
+       public function test (){
+           $this-> load-> view ("layout/header_view");
+           $this-> load-> view ("test_view");
+
+       }
 
 }
+
---
+++ application/views/test_view.php
@@ -0,0 +1,10 @@
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aspernatur autem consequatur dolore, dolorum
+    eveniet facere magnam, nam nisi officiis quae quaerat quas repellendus rerum similique? Eaque magni tempore vero.
+</div>
+<div>Ab delectus excepturi hic iusto maxime molestias perferendis quas repellat repudiandae voluptatum! Accusamus ad
+    adipisci distinctio dolorum ea earum id impedit necessitatibus nemo nisi, non quas reprehenderit rerum, ut vero!
+</div>
+<div>Deserunt impedit inventore ipsum natus, quidem quis reprehenderit temporibus ullam veniam vero. Amet, aperiam
+    cupiditate debitis deleniti et excepturi labore molestiae nisi numquam odio, perferendis placeat ratione
+    reprehenderit ullam voluptate?
+</div>
